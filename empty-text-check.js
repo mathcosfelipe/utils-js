85677d7d22e11e84754fdcbd020e9a8f784592f8
Marcos Felipe <78696211+mathcosfelipe@users.noreply.github.com>
2021-11-11 23:40:36 -0300

    Update empty-text-check.js

--- empty-text-check.js
+++ empty-text-check.js
@@ -1,7 +1,13 @@
-function emptyTextCheck(item_id){ 
-  localStorage.setItem(item_id, item_id);
-  let status = this.
-  if(){
-    
-  }
+function emptyTextCheck(this_id, item_id){ 
+        localStorage.setItem(item_id, document.getElementById(item_id).value);
+        let valor = localStorage.getItem(item_id).toString();
+        let status = document.getElementById(this_id).checked;
+        if(status == false){
+            document.getElementById(item_id).value = '';
+            document.getElementById(item_id).disabled = true;
+        }else{
+            document.getElementById(item_id).disabled = false;
+            document.getElementById(item_id).value = valor;
+            localStorage.removeItem(item_id);
+        }
 }
