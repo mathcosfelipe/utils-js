b983bc95d0a9902c8445e2719bc9a0095309f232
Marcos Felipe <78696211+mathcosfelipe@users.noreply.github.com>
2022-01-18 14:37:07 -0300

    Update download-canvas-png.js

--- download-canvas-png.js
+++ download-canvas-png.js
@@ -1,8 +1,8 @@
-function downloadPng(id_canvas){
+function downloadPng(id_canvas, name_file){
     var canvas = document.getElementById(id_canvas);
     var image = canvas.toDataURL();
     var a_download_link = document.createElement('a');
-    a_download_link.download = 'image.png';
+    a_download_link.download = `{name_file}.png`;
     a_download_link.href = image;
     a_download_link.click();
 }
